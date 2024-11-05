05b274793df1bf98a2c6d6a239e3a0aa0def0c98
mftobin <maura.f.tobin@gmail.com>
2024-11-04 20:55:47 -0600

    updating csv files

---
+++ Maura/schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE heart_disease(
+   LocationAbbr VARCHAR NOT NULL,
+   LocationDesc VARCHAR NOT NULL,
+   GeographicLevel VARCHAR NOT NULL,
+   Data_Value DOUBLE,
+   Stratification1 VARCHAR NOT NULL,
+   Stratification2 VARCHAR NOT NULL,
+   Y_lat DOUBLE,
+   X_lon DOUBLE
+);
